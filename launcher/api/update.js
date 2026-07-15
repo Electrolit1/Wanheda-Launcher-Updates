0d53b6a41433bc5d27d1aebc479d80c65605621b
Electrolit1 <estivenpt2@gmail.com>
2026-07-15 15:28:55 -0400

    Create update.js

---
+++ launcher/api/update.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+
+
+const VERSION_URL =
+"https://raw.githubusercontent.com/TU-USUARIO/Wanheda-Launcher-Updates/main/version.json";
+
+
+
+async function checkVersion(){
+
+    try{
+
+        const response = await axios.get(VERSION_URL);
+
+        return response.data;
+
+    }
+    catch(error){
+
+        console.log(
+        "Error comprobando versión",
+        error
+        );
+
+        return null;
+
+    }
+
+}
+
+
+
+module.exports = {
+    checkVersion
+};
